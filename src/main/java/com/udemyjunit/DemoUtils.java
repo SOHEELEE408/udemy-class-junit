d0192f7943e225fe0a1efa03033b771172d4c5db
SOHEELEE408 <dlwlsrl33@gmail.com>
2023-07-16 17:33:30 +0900

    feat & test : 섹션2-5 더하기 연산 메소드 및 테스트 코드 추가

--- src/main/java/com/udemyjunit/DemoUtils.java
+++ src/main/java/com/udemyjunit/DemoUtils.java
@@ -1,4 +1,8 @@
 package com.udemyjunit;
 
 public class DemoUtils {
+
+    public int add(int a, int b){
+        return a+b;
+    }
 }
--- src/test/java/com/udemyjunit/DemoUtilsTest.java
+++ src/test/java/com/udemyjunit/DemoUtilsTest.java
@@ -1,7 +1,26 @@
 package com.udemyjunit;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DemoUtilsTest {
+
+    @DisplayName("더하기 연산")
+    @Test
+    void testEqualsAndNotEquals() {
+        // given
+        DemoUtils demoUtils = new DemoUtils();
+
+        int expected = 6;
+
+        // when
+        int actual = demoUtils.add(2, 4);
+
+        // then
+        assertEquals(expected, actual, "2+4는 6이다.");
+        
+    }
 
 }
